0598f7f315171ad70097b2e61681b7cb215a5708
Gion Yuki <ninetail.chocolate@gmail.com>
2021-01-09 15:07:00 +0900

    Builded fix

--- msm8953.mk
+++ msm8953.mk
@@ -40,6 +40,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio@6.0-service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.0-impl \
@@ -96,9 +98,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@4.0-impl \
+    android.hardware.graphics.allocator@4.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.mapper@3.0-impl \
+    android.hardware.graphics.mapper@3.0-service \
+    android.hardware.graphics.mapper@4.0-impl \
+    android.hardware.graphics.mapper@4.0-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     copybit.msm8953 \
@@ -346,7 +354,15 @@ PRODUCT_COPY_FILES += \
 # Radio
 PRODUCT_PACKAGES += \
     librmnetctl \
-    libprotobuf-cpp-full
+    libprotobuf-cpp-full \
+    android.hardware.radio@1.5-impl \
+    android.hardware.radio@1.5-service \
+    android.hardware.radio@1.4-impl \
+    android.hardware.radio@1.4-service \
+    android.hardware.radio@1.3-impl \
+    android.hardware.radio@1.3-service \
+    android.hardware.radio@1.2-impl \
+    android.hardware.radio@1.2-service
 
 # AOT Preload
 PRODUCT_DEXPREOPT_SPEED_APPS += \
