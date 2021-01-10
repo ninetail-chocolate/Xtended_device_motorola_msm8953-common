1d42d38b7b1222b0159a85c2448a2e7f807dd2ac
Gion Yuki <ninetail.chocolate@gmail.com>
2021-01-11 04:25:48 +0900

    APEX support

--- msm8953.mk
+++ msm8953.mk
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#APEX
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
